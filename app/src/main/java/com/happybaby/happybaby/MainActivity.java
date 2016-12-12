0351f62fcbdf5bc1257e0f7e4f718aadfa0f99ac
jianquan <527996462@qq.com>
2016-12-12 19:56:37 +0800

    Text2

--- app/src/main/java/com/happybaby/happybaby/MainActivity.java
+++ app/src/main/java/com/happybaby/happybaby/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int x =100;
+        int x =200;
     }
 }
