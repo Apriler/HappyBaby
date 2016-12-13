ab724045dbe9dc6c15e22450bf461740d707663a
Apriler <449790170@qq.com>
2016-12-13 13:45:27 +0800

    分包

--- app/src/main/java/com/happybaby/happybaby/MainActivity.java
+++ app/src/main/java/com/happybaby/happybaby/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int x =200;
-        int y =300;
-        int z= 500;
+
     }
 }
